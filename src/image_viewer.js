4aa7d3cf208530f973867b3c350ab2bb52a2af6f
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2018-01-22 14:23:42 -0600

    6 handling images

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,3 +1,5 @@
+import big from '../assets/big.jpeg';
+import small from '../assets/small.jpeg';
 import '../styles/image_viewer.css';
 
 const image = document.createElement('img');
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path= require('path');
-const ExtractTextPlugin= require('extract-text-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
     entry: './src/index.js',
@@ -8,20 +8,31 @@ const config = {
         filename: 'bundle.js'
     },
     module: {
-        rules : [
-           {
-               use:'babel-loader',
-               test: /\.js$/
-           },
-           {
-            loader: ExtractTextPlugin.extract ({
-                loader: 'css-loader'
-            })   ,
-               test: /\.css$/
-           }
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/
+            },
+            {
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader'
+                }),
+                test: /\.css$/
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: { limit: 40000 }
+                    },
+                    'image-webpack-loader'
+                ]
+
+            }
         ]
     },
-    plugins : [
+    plugins: [
         new ExtractTextPlugin('style.css')
     ]
 }
